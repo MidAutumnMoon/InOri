5eecd798534f5621d3b0de76f0dbcc2636486cdb
MidAutumnMoon <me@418.im>
2024-01-30 16:07:09 +0800

    derputils: fcombine: cleanup

--- derputils/src/bin/fcombine.rs
+++ derputils/src/bin/fcombine.rs
@@ -1,72 +1,72 @@
 use std::path::PathBuf;
+use std::fs::File;
 
 use anyhow::{
-    bail,
-    Result,
+    ensure,
     Context
 };
 
+use tracing::debug;
 
-/// Dump contents of all "input" into "output" without
-/// piping through a shell.
+
+/// Dump contents of <input_files> into <output>
+/// without a shell.
 #[derive( argh::FromArgs, Debug )]
 struct CmdOptions {
-    /// a place to dump contents into
+    /// file to write into, can't be an existsing one
     #[argh( positional )]
     output: PathBuf,
 
-    /// whose contents will be combined
+    /// source of read contents
     #[argh( positional )]
-    files: Vec<PathBuf>
+    input_files: Vec<PathBuf>
 }
 
 
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
+
+    // Init tracing
+
+    ino_tracing::init_tracing_subscriber();
+
 
     // Acquire command line options.
 
-    let CmdOptions { output, files } = argh::from_env();
+    let CmdOptions { output, input_files } = argh::from_env();
 
+    debug!( ?output, ?input_files );
 
     // Avoid accidents
 
-    if output.try_exists()? {
-        bail!(
-            "\"{}\" already exists",
-            output.display()
-        )
+    ensure! { ! output.try_exists()?,
+        "\"{}\" already exists",
+        output.display()
     }
 
 
     // Do the IO works
 
-    use std::fs::File;
+    debug!( "Create output file" );
 
-    let mut output = {
-        let message = || format! {
-            "Failed writing \"{}\"",
-            &output.display()
-        };
-        File::create( &output ).with_context( message )?
-    };
+    let mut output = File::create( output )
+        .context( "Failed creating output file" )?;
 
+    for file in input_files {
 
-    for file in files {
+        debug!( ?file, "Read input file" );
 
-        let mut input = {
-            let message = || format! {
-                "Failed writing \"{}\"",
-                &file.display()
-            };
-            File::open( &file ).with_context( message )?
-        };
+        let mut input = File::open( &file )
+            .with_context( || format!(
+                "Failed reading \"{}\"", &file.display()
+            ) )?;
 
         std::io::copy( &mut input, &mut output )
-            .with_context( || format!{ "Error copying \"{}\"", &file.display() } )?;
+            .with_context( || format!(
+                "Error to copy \"{}\"", &file.display()
+            ) )?;
 
     }
 
-
-    Ok( () )
+    Ok(())
 
 }
