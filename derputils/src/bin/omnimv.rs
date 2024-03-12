1ac04e5e6f0559cf96cca6f045637c31bcfa3161
MidAutumnMoon <me@418.im>
2024-03-12 18:07:41 +0800

    derputils: omnimv: refactor

--- derputils/src/bin/omnimv.rs
+++ derputils/src/bin/omnimv.rs
@@ -5,16 +5,27 @@
 //! to CWD, so "omnifetch" may be a more
 //! proper name but... whatever.
 
-use std::path::PathBuf;
+use std::path::{
+    Path,
+    PathBuf,
+};
 
 use tracing::{
     debug,
     debug_span,
 };
 
-use anyhow::ensure;
+use anyhow::{
+    ensure,
+    bail,
+};
 
 use itertools::Itertools;
+
+
+/// Arbitrary value based on the current amount
+/// of files in my download folder :P
+const AVERAGE_AMOUNT: usize = 70;
 
 
 /// Move files from other places to current
@@ -28,7 +39,6 @@ struct CmdOpts {
 
     /// list filenames in configured directories,
     /// useful for doing shell completion.
-    /// Note: results are deduplicated
     #[argh( switch, short = 'l' )]
     listing: bool,
 
@@ -38,224 +48,230 @@ struct CmdOpts {
     /// in multiple search directories,
     /// only the first one will be moved.
     #[argh( positional, arg_name = "filenames" )] // arg_name undocumented
-    files_to_move: Vec<PathBuf>,
+    feedle_names: Vec<String>,
 }
 
 impl CmdOpts {
-
     #[tracing::instrument]
     fn new() -> anyhow::Result<Self> {
         let opts = argh::from_env::<Self>();
 
-        debug!( ?opts, "Parsed cmdopts" );
+        debug!( ?opts, "cmdopts" );
 
         ensure! { ! opts.searchdirs.is_empty(),
             "At least one --dir must be specified.\
             \n\n\
-            Run omnimv --help for more information."
+            Run with --help for more information."
         }
 
-        // dedup searchdirs
+        ensure! { opts.listing || ! opts.feedle_names.is_empty(),
+            "No files to be moved.\
+            \n\n\
+            Run with --help for more information."
+        }
+
         let searchdirs = opts.searchdirs.clone()
             .into_iter()
             .unique()
             .collect_vec();
 
-        debug!( ?searchdirs, "Deduped search dirs" );
+        debug!( ?searchdirs, "deduped search dirs" );
 
         Ok( Self { searchdirs, ..opts } )
     }
+}
 
+/// Feedle: file needle
+#[derive( Debug )]
+struct Feedle {
+    name: String,
+    origin: PathBuf,
+}
+
+impl Feedle {
+    /// Helper function for collecting Feedles from `dir`.
+    #[tracing::instrument]
+    fn from_dir( dir: &Path )
+        -> anyhow::Result<Vec<Self>>
+    {
+        debug!( "looking for files" );
+
+        let mut collected =
+            Vec::with_capacity( AVERAGE_AMOUNT );
+
+        for entry in dir.read_dir()? {
+            let _s = debug_span!( "maybe_entry", ?entry ).entered();
+            let entry = entry?;
+
+            // ignore symlinks for now
+            if entry.file_type()?.is_file() {
+                debug!( "found file" );
+                let name = entry.file_name()
+                    .to_string_lossy()
+                    .into_owned();
+                let origin = entry.path();
+                collected.push( Self { name, origin } )
+            } else {
+                debug!( "not file, skip" );
+                continue;
+            }
+        }
+        Ok( collected )
+    }
+
+    #[tracing::instrument]
+    fn move_to( &self, dest: &Path ) -> anyhow::Result<()> {
+        use std::process::Command;
+        debug!( "move file" );
+
+        println!( "Move \"{}\"", self.origin.display() );
+
+        let result = Command::new( "mv" )
+            .args([ "--verbose", "--no-clobber" ])
+            .arg( self.origin.as_path() )
+            .arg( dest )
+            .output()?;
+
+        debug!( ?result, "command result" );
+
+        ensure! { result.status.success(),
+            "Move failed\n\nStderr: {}",
+            String::from_utf8_lossy( &result.stderr ).trim()
+        }
+        Ok(())
+    }
+}
+
+
+#[derive( Debug )]
+struct Haystack {
+    inner: Vec<Feedle>,
+}
+
+impl Haystack {
+    #[tracing::instrument]
+    fn new() -> Self {
+        Self { inner: Vec::with_capacity( AVERAGE_AMOUNT * 2 ) }
+    }
+
+    #[tracing::instrument]
+    fn append( &mut self, other: &mut Vec<Feedle> ) {
+        self.inner.append( other )
+    }
+
+    #[tracing::instrument]
+    fn feedle_names( &self ) -> Vec<&str> {
+        self.inner.iter()
+            .map( |e| e.name.as_str() )
+            .collect()
+    }
+
+    #[tracing::instrument]
+    fn find( &self, name: &str ) -> Vec<&Feedle> {
+        self.inner.iter()
+            .filter( |e| e.name == name )
+            .collect_vec()
+    }
 }
 
 
 fn main() -> anyhow::Result<()> {
 
-    //
     // Enable tracing
-    //
 
     ino_tracing::init_tracing_subscriber();
 
 
-    //
     // Get cmd options
-    //
 
     let opts = CmdOpts::new()?;
 
     let CmdOpts {
-        files_to_move,
+        feedle_names,
         searchdirs,
         ..
     } = &opts;
 
 
-    //
-    // Listing mode
-    //
+    // Collect feedles
 
-    if opts.listing {
-        let _span = debug_span!( "listing" ).entered();
+    let haystack: Haystack = {
+        let _s = debug_span!( "haystack" ).entered();
 
-        debug!( "Listing files" );
+        debug!( "collect feedles to make haystack" );
 
-        // "70" is an arbitrary value based on
-        // the current number of files in my download
-        // folder :P, and also it's about the average amount.
-        let mut collected =
-            Vec::<String>::with_capacity( 70 );
+        let mut haystack = Haystack::new();
 
-        for srchdir in searchdirs {
-            let _span = debug_span!( "inside", ?srchdir ).entered();
-
-            debug!( "Try read_dir" );
-
-            for entry in srchdir.read_dir()? {
-                let _span = debug_span!( "on_entry", ?entry ).entered();
-
-                let entry = entry?;
-                let ftype = &entry.file_type()?;
-
-                // Not considering for symlinks,
-                // they're not common in my workflow.
-                // May be tweakable in the future.
-                if ! ftype.is_file() {
-                    debug!( "Not file, skip" );
-                    continue
-                }
-
-                let fname = entry.file_name()
-                    .to_string_lossy()
-                    .into_owned();
-                debug!( ?fname, "Found file" );
-                collected.push( fname );
-            }
+        for schdir in searchdirs {
+            let _s = debug_span!( "inside", ?schdir ).entered();
+            haystack.append(
+                &mut Feedle::from_dir( schdir )?
+            );
         }
 
-        let output = collected.into_iter()
-            .unique()
-            .join( "\n" );
+        haystack
+    };
 
-        println!( "{output}" );
+    debug!( ?haystack, "final haystack" );
+
+
+    // Listing
+
+    if opts.listing {
+        let _s = debug_span!( "listing" ).entered();
+        debug!( "listing mode" );
+
+        println! { "{}",
+            haystack.feedle_names().join( "\n" )
+        };
 
         return Ok(())
     }
 
 
-    //
-    // Actually moving files
-    //
+    // Moving files
 
-    let _span = debug_span!( "moving" ).entered();
+    let _s = debug_span!( "moving" ).entered();
 
+    debug!( ?feedle_names );
 
-    debug!( ?files_to_move, "Files to move" );
+    let current_dir = std::env::current_dir()?;
 
-    debug!( "Try to get CWD" );
-
-    let cwd = std::env::current_dir()?;
-
-    debug!( ?cwd );
+    debug!( ?current_dir );
 
 
-    if files_to_move.is_empty() {
-        debug!( "No files to move" )
+    for name in feedle_names {
+        let _s = debug_span!( "feedle_name", ?name ).entered();
+
+        let found = match haystack.find( name )[..] {
+            [] => {
+                debug!( "not found" );
+                bail!( "File \"{name}\" not found in searchdirs" );
+            },
+            [ feedle ] => {
+                debug!( "found one" );
+                feedle
+            },
+            ref all @ [ .. ] => {
+                debug!( "find multiple, take first" );
+                all.first().unwrap()
+            },
+        };
+
+        debug!( ?found );
+
+        let feedle_dest = current_dir.join( name );
+
+        debug!( ?feedle_dest, "prepare to move" );
+
+        debug!( "check for collinsion" );
+
+        ensure! { ! feedle_dest.try_exists()?,
+            "{name} already exists under current directory",
+        };
+
+        found.move_to( &feedle_dest )?
     }
-
-    for filename in files_to_move {
-        let _span = debug_span!( "for_name", ?filename ).entered();
-
-
-        let mv_dest = cwd.join( filename );
-
-        debug!( "Ensure no collinsion" );
-
-        ensure! { ! mv_dest.try_exists()?,
-            "\"{}\" already exists under CWD",
-            filename.display(),
-        }
-
-
-        let mut collected = Vec::<PathBuf>::new();
-
-        for srchdir in searchdirs {
-            let _span = debug_span!( "search_in", ?srchdir ).entered();
-
-            let path = srchdir.join( filename );
-
-            debug!( ?path, "Try path" );
-
-            if path.try_exists()? {
-                debug!( ?path, "Found" );
-                collected.push( path )
-            } else {
-                debug!( "Not exists" )
-            }
-        }
-
-        debug!( ?collected, "Collected source files" );
-
-
-        ensure! { ! collected.is_empty(),
-            "\"{}\" not found in specified directories.",
-            filename.display()
-        }
-
-        if collected.len() > 1 {
-            debug!( "Found duplicated files, take first" )
-        }
-
-        // Safety: bails when empty, so "collected"
-        // guaranteed to hold at least one item
-        let mv_orig = &collected.first().unwrap();
-
-
-        let _span_from_to = debug_span!( "from_to", ?mv_orig, ?mv_dest )
-            .entered();
-
-        debug!( "Ready to move" );
-
-        println!( "Move {} -> CWD ({})",
-            &mv_orig.display(),
-            &cwd.display()
-        );
-
-        // coreutils is more well equiped
-        // to deal with all sorts of edge cases.
-        //
-        // Namely:
-        //
-        // 1) rename*() doesn't work across mountpoints
-        //
-        // 2) Rust's fs::copy will try to
-        // preserve metadata with no way to control
-        // which will fail on some NFS or WSL's
-        // drvfs on which linux permission is not
-        // available.
-        //
-        // 3) Some deadly wild animals sneaking behind
-        // the read() and write() syscalls.
-
-        use std::process::Command;
-
-        debug!( "Calling mv to move files" );
-
-        let output = Command::new( "mv" )
-            .args([ "--verbose", "--no-clobber" ])
-            .arg( mv_orig )
-            .arg( mv_dest )
-            .output()?;
-
-        debug!( ?output, "mv output" );
-
-        ensure! { output.status.success(),
-            "Move failed.\n\n{}",
-            String::from_utf8_lossy( &output.stderr ).trim()
-        }
-    }
-
 
     Ok(())
-
 }
