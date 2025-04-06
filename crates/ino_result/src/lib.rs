2135cfc169d67ddf0698640b877b16da2709c0f4
MidAutumnMoon <me@418.im>
2025-04-06 13:49:28 +0800

    ino_result: split the function into two parts

--- crates/ino_result/src/lib.rs
+++ crates/ino_result/src/lib.rs
@@ -1,22 +1,27 @@
 //! Extension methods for [`Result`]
 
-pub trait ResultExt<T, E> {
-    /// Print Err to stderr and exit the program, or unwrap if Ok.
-    fn unwrap_print_error( self ) -> T;
+pub trait ResultExt<OK, ERR> {
+    /// Peek at the Err and print it to stderr.
+    fn print_error( self ) -> Self;
+
+    /// Print Err and exit the program, or unwrap the value if Ok.
+    fn unwrap_print_error( self ) -> OK;
 }
 
-impl<T, E> ResultExt<T, E> for Result<T, E>
+impl<OK, ERR> ResultExt<OK, ERR> for Result<OK, ERR>
 where
-    E: std::fmt::Debug
+    ERR: std::fmt::Debug
 {
-    #[ inline ]
-    fn unwrap_print_error( self ) -> T {
-        match self {
-            Err( e ) => {
-                eprintln!( "{e:?}" );
-                std::process::exit( 1 );
-            }
-            Ok( o ) => o,
-        }
+    #[ inline( always ) ]
+    fn print_error( self ) -> Self {
+        self.inspect_err( |err| eprintln!( "{err:?}" ) )
+    }
+
+    #[ inline( always ) ]
+    fn unwrap_print_error( self ) -> OK {
+        self
+            .print_error()
+            .map_err( |_| std::process::exit( 1 ) )
+            .unwrap()
     }
 }
