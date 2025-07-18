4b4b1a7626a257e24b1c8ac30034a1c70008729c
MidAutumnMoon <me@418.im>
2025-07-18 21:57:52 +0800

    avxl: get ride of most logic

--- avxl/src/main.rs
+++ avxl/src/main.rs
@@ -6,6 +6,7 @@ use std::path::{
 use std::process::ExitStatus;
 
 use anyhow::Result as AnyResult;
+use ino_tap::TapExt;
 use tracing::{
     debug,
     debug_span,
@@ -24,6 +25,12 @@ pub const ARCHIVE_DIR_NAME: &str = ".backup";
 #[ derive( clap::Args, Debug ) ]
 struct CliInput {
     dir_and_files: Vec<PathBuf>
+}
+
+#[ derive( clap::Args, Debug ) ]
+struct CommonCliOpts {
+    #[ arg( long, short ) ]
+    working_dir: Option<PathBuf>,
 }
 
 /// Batch converting pictures between formats.
@@ -54,12 +61,30 @@ enum CliOpts {
     },
 }
 
+impl CliOpts {
+    #[ tracing::instrument( name="cliopts_parse" ) ]
+    fn parse() -> Self {
+        <Self as clap::Parser>::parse().tap_trace()
+    }
+}
+
 struct App {
-    cliopts: CliOpts,
+    transcoder: Box<dyn Transcoder>,
     working_dir: PathBuf,
 }
 
-impl App {}
+impl TryFrom<CliOpts> for App {
+    type Error = anyhow::Error;
+
+    #[ tracing::instrument( name="app_from_cliopts" ) ]
+    fn try_from( cliopts: CliOpts ) -> AnyResult<Self> {
+        use CliOpts::*;
+        // match cliopts {
+        //     Avif
+        // }
+        todo!()
+    }
+}
 
 #[ derive( Debug ) ]
 pub enum DirOrFiles {
@@ -77,217 +102,210 @@ trait Transcoder {
     fn transcode( &self, source: &Path ) -> AnyResult<ExitStatus>;
 }
 
-pub struct Task {
-    source: PathBuf,
-    output: PathBuf,
-}
-
 fn main() -> AnyResult<()> {
 
     fn main_but_result() {}
 
     ino_tracing::init_tracing_subscriber();
 
-    let cliopts = < CliOpts as clap::Parser >::parse();
+    let cliopts = CliOpts::parse();
 
-    debug!( ?cliopts );
-
-    let ( encoder, dir_and_files ): ( Box<dyn Transcoder>, _ ) = match cliopts {
-        CliOpts::Avif { avif, input } => {
-            debug!( "AVIF mode" );
-            ( Box::new( avif ), input.dir_and_files )
-        },
-        CliOpts::Jxl { input } => {
-            debug!( "JXL mode" );
-            ( Box::new( jxl::Jxl ), input.dir_and_files )
-        },
-        CliOpts::Despeckle { despeckle, input } => {
-            debug!( "Despeckle" );
-            ( Box::new( despeckle ), input.dir_and_files )
-        }
-    };
+    // let ( encoder, dir_and_files ): ( Box<dyn Transcoder>, _ ) = match cliopts {
+    //     CliOpts::Avif { avif, input } => {
+    //         debug!( "AVIF mode" );
+    //         ( Box::new( avif ), input.dir_and_files )
+    //     },
+    //     CliOpts::Jxl { input } => {
+    //         debug!( "JXL mode" );
+    //         ( Box::new( jxl::Jxl ), input.dir_and_files )
+    //     },
+    //     CliOpts::Despeckle { despeckle, input } => {
+    //         debug!( "Despeckle" );
+    //         ( Box::new( despeckle ), input.dir_and_files )
+    //     }
+    // };
 
 
     /*
      * Sanitize input
      */
 
-    let dir_and_files = if dir_and_files.is_empty() {
-        debug!( "CLI provided input is empty, use PWD" );
-        vec![ std::env::current_dir()? ]
-    } else {
-        dir_and_files
-    };
-
-    let dir_and_files: Vec<DirOrFiles> = {
-        let mut dirs: Vec<PathBuf> = vec![];
-        let mut files: Vec<PathBuf> = vec![];
-
-        for it in dir_and_files {
-            if it.is_dir() {
-                let Some( basename ) = it.file_name() else { continue; };
-                // skip the dir created by ourselves.
-                if basename == ARCHIVE_DIR_NAME {
-                    eprintln!(
-                        "Skipping dir \"{}\" because it's named {ARCHIVE_DIR_NAME} \
-                        which is used for storing original files after encoding.\
-                        \n\
-                        This should be a mistake, otherwise rename the directory \
-                        to another name.",
-                        it.display()
-                    );
-                    continue;
-                }
-                dirs.push( it );
-            } else if it.is_file() {
-                files.push( it );
-            } else {
-                eprintln!(
-                    "\"{}\" is not a file nor dir, which is not supported.",
-                    it.display()
-                );
-                std::process::exit( 1 )
-            }
-        }
-
-        Vec::with_capacity( dirs.len() + 1 )
-            .tap_mut( |s| {
-                let mut dirs = dirs.into_iter()
-                    .map( DirOrFiles::Dir )
-                    .collect();
-                s.append( &mut dirs );
-            } )
-            .tap_mut( |s| {
-                s.push( DirOrFiles::Files( files ) );
-            } )
-    };
-
-    debug!( ?dir_and_files );
+    // let dir_and_files = if dir_and_files.is_empty() {
+    //     debug!( "CLI provided input is empty, use PWD" );
+    //     vec![ std::env::current_dir()? ]
+    // } else {
+    //     dir_and_files
+    // };
+    //
+    // let dir_and_files: Vec<DirOrFiles> = {
+    //     let mut dirs: Vec<PathBuf> = vec![];
+    //     let mut files: Vec<PathBuf> = vec![];
+    //
+    //     for it in dir_and_files {
+    //         if it.is_dir() {
+    //             let Some( basename ) = it.file_name() else { continue; };
+    //             // skip the dir created by ourselves.
+    //             if basename == ARCHIVE_DIR_NAME {
+    //                 eprintln!(
+    //                     "Skipping dir \"{}\" because it's named {ARCHIVE_DIR_NAME} \
+    //                     which is used for storing original files after encoding.\
+    //                     \n\
+    //                     This should be a mistake, otherwise rename the directory \
+    //                     to another name.",
+    //                     it.display()
+    //                 );
+    //                 continue;
+    //             }
+    //             dirs.push( it );
+    //         } else if it.is_file() {
+    //             files.push( it );
+    //         } else {
+    //             eprintln!(
+    //                 "\"{}\" is not a file nor dir, which is not supported.",
+    //                 it.display()
+    //             );
+    //             std::process::exit( 1 )
+    //         }
+    //     }
+    //
+    //     Vec::with_capacity( dirs.len() + 1 )
+    //         .tap_mut( |s| {
+    //             let mut dirs = dirs.into_iter()
+    //                 .map( DirOrFiles::Dir )
+    //                 .collect();
+    //             s.append( &mut dirs );
+    //         } )
+    //         .tap_mut( |s| {
+    //             s.push( DirOrFiles::Files( files ) );
+    //         } )
+    // };
+    //
+    // debug!( ?dir_and_files );
 
 
     /*
      * Tasks and encoding
      */
 
-    let _span_of_daf =
-        debug_span!( "encode_dir_and_files" ).entered();
-
-    for dir_or_files in dir_and_files {
-
-        debug!( ?dir_or_files );
-
-        let archive_after_encode: bool;
-        let archive_dir: Option<PathBuf>;
-
-        let files_to_encode: Vec<PathBuf>;
-
-        /*
-         * Unwrap dir_and_files to construct tasks
-         */
-
-        match dir_or_files {
-            // If it is a dir, enable archive_after_encode
-            // and collect files inside it
-            DirOrFiles::Dir( dir ) => {
-                eprintln!(
-                    "Checking directory {}", dir.display()
-                );
-                archive_after_encode = true;
-                archive_dir = Some( dir.join( ARCHIVE_DIR_NAME ) );
-                files_to_encode = tool::filter_by_supported_exts(
-                    &encoder, tool::find_files( &dir )?
-                );
-
-            },
-            // If it is file otherwise, the files are already the tasks.
-            DirOrFiles::Files( files ) => {
-                let files = tool::filter_by_supported_exts( &encoder, files );
-                // ...so that app won't print "Checking 0 files"
-                if files.is_empty() { continue }
-                eprintln!(
-                    "Chekcing {} files", files.len()
-                );
-                archive_after_encode = false;
-                archive_dir = None;
-                files_to_encode = files;
-            }
-        }
-
-        debug!( ?files_to_encode, ?archive_after_encode, ?archive_dir );
-
-        if files_to_encode.is_empty() {
-            eprintln!( "No file need to be encoded" );
-            continue;
-        }
-
-
-        /*
-         * Create archive_dir is needed
-         */
-
-        if archive_after_encode {
-            debug!( ?archive_dir );
-
-            // UNWRAP: when archive_after_encode is set archive_dir is also set
-            #[ allow( clippy::unwrap_used ) ]
-            let dir = archive_dir.clone().unwrap();
-
-            eprintln!(
-                "Archive after encoding\
-                \n\
-                Create directory \"{}\"for archiving",
-                dir.display()
-            );
-
-            if !dir.try_exists()? {
-                std::fs::create_dir_all( dir )?;
-            }
-        }
-
-        /*
-         * Do collected tasks
-         */
-
-        let total_tasks = files_to_encode.len();
-
-        for ( index, file ) in files_to_encode.iter().enumerate() {
-            debug!( ?index, ?file );
-
-            let _span = debug_span!( "encoding_tasks", ?file ).entered();
-
-            let progress_percent = format!(
-                "[{}/{total_tasks} {}]",
-                index + 1,
-                file.file_name()
-                    .unwrap_or_default()
-                    .to_string_lossy(),
-            );
-
-            eprintln!(
-                "{progress_percent} Encode in progress..."
-            );
-
-            let encode_status = encoder.transcode( file )?;
-
-            if !encode_status.success() {
-                eprintln!(
-                    "{progress_percent} Failed to encode!"
-                );
-                std::process::exit( 1 )
-            }
-
-            if archive_after_encode {
-                eprintln!( "{progress_percent} Archive original file");
-                let basename = file.file_name()
-                    .expect( "It doesn't have a basename, how come?!" );
-                // TODO: this is code smell, do something later
-                #[ allow( clippy::unwrap_used ) ]
-                let target = archive_dir.clone().unwrap().join( basename );
-                std::fs::rename( file, target )?;
-            }
-        }
-
-    }
+    // let _span_of_daf =
+    //     debug_span!( "encode_dir_and_files" ).entered();
+    //
+    // for dir_or_files in dir_and_files {
+    //
+    //     debug!( ?dir_or_files );
+    //
+    //     let archive_after_encode: bool;
+    //     let archive_dir: Option<PathBuf>;
+    //
+    //     let files_to_encode: Vec<PathBuf>;
+    //
+    //     /*
+    //      * Unwrap dir_and_files to construct tasks
+    //      */
+    //
+    //     match dir_or_files {
+    //         // If it is a dir, enable archive_after_encode
+    //         // and collect files inside it
+    //         DirOrFiles::Dir( dir ) => {
+    //             eprintln!(
+    //                 "Checking directory {}", dir.display()
+    //             );
+    //             archive_after_encode = true;
+    //             archive_dir = Some( dir.join( ARCHIVE_DIR_NAME ) );
+    //             files_to_encode = tool::filter_by_supported_exts(
+    //                 &encoder, tool::find_files( &dir )?
+    //             );
+    //
+    //         },
+    //         // If it is file otherwise, the files are already the tasks.
+    //         DirOrFiles::Files( files ) => {
+    //             let files = tool::filter_by_supported_exts( &encoder, files );
+    //             // ...so that app won't print "Checking 0 files"
+    //             if files.is_empty() { continue }
+    //             eprintln!(
+    //                 "Chekcing {} files", files.len()
+    //             );
+    //             archive_after_encode = false;
+    //             archive_dir = None;
+    //             files_to_encode = files;
+    //         }
+    //     }
+    //
+    //     debug!( ?files_to_encode, ?archive_after_encode, ?archive_dir );
+    //
+    //     if files_to_encode.is_empty() {
+    //         eprintln!( "No file need to be encoded" );
+    //         continue;
+    //     }
+    //
+    //
+    //     /*
+    //      * Create archive_dir is needed
+    //      */
+    //
+    //     if archive_after_encode {
+    //         debug!( ?archive_dir );
+    //
+    //         // UNWRAP: when archive_after_encode is set archive_dir is also set
+    //         #[ allow( clippy::unwrap_used ) ]
+    //         let dir = archive_dir.clone().unwrap();
+    //
+    //         eprintln!(
+    //             "Archive after encoding\
+    //             \n\
+    //             Create directory \"{}\"for archiving",
+    //             dir.display()
+    //         );
+    //
+    //         if !dir.try_exists()? {
+    //             std::fs::create_dir_all( dir )?;
+    //         }
+    //     }
+    //
+    //     /*
+    //      * Do collected tasks
+    //      */
+    //
+    //     let total_tasks = files_to_encode.len();
+    //
+    //     for ( index, file ) in files_to_encode.iter().enumerate() {
+    //         debug!( ?index, ?file );
+    //
+    //         let _span = debug_span!( "encoding_tasks", ?file ).entered();
+    //
+    //         let progress_percent = format!(
+    //             "[{}/{total_tasks} {}]",
+    //             index + 1,
+    //             file.file_name()
+    //                 .unwrap_or_default()
+    //                 .to_string_lossy(),
+    //         );
+    //
+    //         eprintln!(
+    //             "{progress_percent} Encode in progress..."
+    //         );
+    //
+    //         let encode_status = encoder.transcode( file )?;
+    //
+    //         if !encode_status.success() {
+    //             eprintln!(
+    //                 "{progress_percent} Failed to encode!"
+    //             );
+    //             std::process::exit( 1 )
+    //         }
+    //
+    //         if archive_after_encode {
+    //             eprintln!( "{progress_percent} Archive original file");
+    //             let basename = file.file_name()
+    //                 .expect( "It doesn't have a basename, how come?!" );
+    //             // TODO: this is code smell, do something later
+    //             #[ allow( clippy::unwrap_used ) ]
+    //             let target = archive_dir.clone().unwrap().join( basename );
+    //             std::fs::rename( file, target )?;
+    //         }
+    //     }
+    //
+    // }
 
     Ok(())
 
