0fb64d8f5dd565a8973d371720c741a4676fcd86
MidAutumnMoon <me@418.im>
2025-09-02 18:38:28 +0800

    noah: prepare for refactor

--- noah/src/main.rs
+++ noah/src/main.rs
@@ -1,18 +1,24 @@
 mod checks;
 mod clean;
+mod cli;
 mod commands;
 mod completion;
 mod generations;
 mod installable;
-mod cli;
 mod json;
 mod logging;
 mod nixos;
 mod update;
 mod util;
 
+use color_eyre::eyre::Context;
 // TODO: get rid of eyre
 use color_eyre::Result;
+use color_eyre::Result as EyreResult;
+use semver::Version;
+
+// const MINIMUM_NIX_VERSION: Version = Version::new(2, 28, 4);
+const MINIMUM_LIX_VERSION: Version = Version::new(2, 93, 3);
 
 fn main() -> Result<()> {
     let args = <crate::cli::CliOpts as clap::Parser>::parse();
@@ -20,6 +26,8 @@ fn main() -> Result<()> {
     // Set up logging
     crate::logging::setup_logging(args.verbosity)?;
     tracing::debug!("{args:#?}");
+
+    startup_check().context("Failed to run startup checks")?;
 
     // Check Nix version upfront
     checks::verify_nix_environment()?;
@@ -30,4 +38,8 @@ fn main() -> Result<()> {
     checks::verify_variables()?;
 
     args.command.run()
+}
+
+fn startup_check() -> EyreResult<()> {
+    todo!()
 }
