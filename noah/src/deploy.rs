8d2e840f0248c16da869bcd0015403243ff39cc5
MidAutumnMoon <me@418.im>
2025-09-03 12:51:33 +0800

    noah: add options to deploy

--- noah/src/deploy.rs
+++ noah/src/deploy.rs
@@ -6,6 +6,10 @@ use crate::nixos::BuildOpts;
 #[derive(Debug)]
 #[derive(clap::Args)]
 pub struct Deploy {
+    #[arg(long, short)]
+    #[arg(value_name = "NAME")]
+    exclude_host: Option<Vec<String>>,
+
     #[command(flatten)]
     build_opts: BuildOpts,
 }
