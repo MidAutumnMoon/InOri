c1f6ca7c4379422fcdb3a8bfcf1d2197cbc7e6e0
MidAutumnMoon <me@418.im>
2026-06-21 13:40:07 +0800

    imgo: Fix Clippy lints in test PNG generator

--- imgo/examples/gen_test_png.rs
+++ imgo/examples/gen_test_png.rs
@@ -1,11 +1,17 @@
 use image::ImageBuffer;
 use image::Rgba;
 
+#[expect(clippy::unwrap_used)]
 fn main() {
     let (w, h) = (37u32, 23u32);
     let img: ImageBuffer<Rgba<u8>, Vec<u8>> =
         ImageBuffer::from_fn(w, h, |x, y| {
-            Rgba([(x * 7) as u8, (y * 11) as u8, (x ^ y) as u8, 255])
+            Rgba([
+                u8::try_from(x * 7).expect("x*7 fits u8"),
+                u8::try_from(y * 11).expect("y*11 fits u8"),
+                u8::try_from(x ^ y).expect("x^y fits u8"),
+                255,
+            ])
         });
     let out = std::env::args().nth(1).unwrap_or_else(|| "orig.png".into());
     img.save(&out).unwrap();
