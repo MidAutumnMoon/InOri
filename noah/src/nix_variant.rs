34e7e1dd2e89344c7d7afcd2b398726902578c20
MidAutumnMoon <me@418.im>
2026-08-13 00:59:57 +0800

    noah: migrate to ino_shell

--- noah/src/nix_variant.rs
+++ noah/src/nix_variant.rs
@@ -1,6 +1,6 @@
+use ino_shell::Shell;
 use rootcause::Report;
 use rootcause::prelude::ResultExt;
-use xshell::Shell;
 
 /// Variant of the system Nix, notice that detsys Nix is not supported.
 pub enum NixVariant {
@@ -10,7 +10,7 @@ pub enum NixVariant {
 
 pub fn nix_variant() -> rootcause::Result<NixVariant> {
     let shell = Shell::new()?;
-    let version_output = xshell::cmd!(shell, "nix --version")
+    let version_output = ino_shell::cmd!(shell, "nix --version")
         .read()
         .context("Failed to run nix --version")?;
     println!("{version_output}");
